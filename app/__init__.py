e8770979afa01e4f0c1e4004ae9aab6b1dd57a1c
EmanuelAlmonte <emanuelalmonte2007@gmail.com>
2024-11-27 19:53:28 -0400

    Cambiar html, css y en __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if request.method == "POST":
+        Nombre = request.form["Nombre"]
+        Contraseña = request.form["password"]
+        
+        if Nombre and Contraseña:
+            return redirect(url_for("chat.html",user= Nombre))
+        else:
+            return None  
     return render_template("index.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
