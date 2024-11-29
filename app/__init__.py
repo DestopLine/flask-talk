738576bd4260e0781812b7b28cf89c13c132b8e5
EmanuelAlmonte <emanuelalmonte2007@gmail.com>
2024-11-29 19:58:05 -0400

    Cambiar login y app.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/login")
-def login():
+@app.route("/")
+def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username and password:
+            return redirect(url_for("index"))
+        else:
+            return "<h1>Intento de inicio de sesion fallido, intentar de nuevo</h1>"
+    return render_template("login.html")
+
+@app.route("/post", methods=["POST"])
+def create_post():
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
