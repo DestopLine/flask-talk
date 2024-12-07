14d487c801a5dc28a86232e5359efab05ad315c7
DestopLine <destopline555@hotmail.com>
2024-12-06 20:31:38 -0400

    Mostrar cantidad de respuestas

--- app/static/scripts/publicaciones.js
+++ app/static/scripts/publicaciones.js
@@ -10,14 +10,15 @@ for (let comment of comments) {
 function addReplyToggleFeature(comment) {
 	let toggleButton = comment.querySelector(".toggle-respuestas-btn");
 	let replies = comment.querySelector(".replies");
+	const replyAmount = replies.children.length;
 
 	toggleButton.addEventListener("click", (_) => {
 		if (replies.classList.contains("hidden")) {
 			replies.classList.remove("hidden");
-			toggleButton.textContent = "Ocultar respuestas";
+			toggleButton.textContent = `Ocultar ${replyAmount} respuestas`;
 		} else {
 			replies.classList.add("hidden");
-			toggleButton.textContent = "Mostrar respuestas";
+			toggleButton.textContent = `Mostrar ${replyAmount} respuestas`;
 		}
 	});
 }
